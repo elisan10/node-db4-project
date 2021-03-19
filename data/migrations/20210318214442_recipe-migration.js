d4b57fb1a751f3fddcd6e31f165e0991e06bb9a5
Eli <elize.san10@gmail.com>
2021-03-18 19:07:25 -0500

    create tables in migration file

--- data/migrations/20210318214442_recipe-migration.js
+++ data/migrations/20210318214442_recipe-migration.js
@@ -1,8 +1,53 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("recipes", (tbl) => {
+      tbl.increments("recipe_id");
+      tbl.string("recipe_name", 128).notNullable().unique();
+      tbl.timestamp("created_at").defaultTo(knex.fn.now());
+    })
+    .createTable("steps", (tbl) => {
+      tbl.increments("step_id");
+      tbl.integer("step_number");
+      tbl.text("step_instructions").notNullable();
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("recipe_id")
+        .inTable("recipes")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("step_ingredients", (tbl) => {
+      tbl.increments("step_ingredient_id");
+      tbl.decimal("quantity").notNullable();
+      tbl
+        .integer("ingredient_id")
+        .unsigned()
+        .notNullable()
+        .references("ingredient_id")
+        .inTable("ingredients")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("step_id")
+        .unsigned()
+        .notNullable()
+        .references("step_id")
+        .inTable("steps")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    })
+    .createTable("ingredients", (tbl) => {
+      tbl.increments("ingredient_id");
+      tbl.string("ingredient_name", 128).notNullable().unique();
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("step_ingredients")
+    .dropTableIfExists("steps")
+    .dropTableIfExists("recipes");
 };
